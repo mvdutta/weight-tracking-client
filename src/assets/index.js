c32c0084014b0669e6189b1ad890a83674f82a9d
Maia Dutta <mermaia33@gmail.com>
2023-03-17 14:46:53 -0500

    styled CNA dashboard cards

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,13 @@
 import menu from "./menu-icon2.png";
 import close from "./close-icon3.png";
+import scale from "./clipboard-1.png";
+import list from "./census-icon1.png";
+import message from "./message-icon1.png";
 
 export { 
   menu,
   close,
+  scale,
+  list,
+  message,
 };
--- src/components/dashboard/CNAdashboard.js
+++ src/components/dashboard/CNAdashboard.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../nav/NavBar";
 import "./Dashboard.css"
+import { scale, list, message } from "../../assets";
 
 const CNAdashboard = () => {
   const navigate = useNavigate();
@@ -20,64 +21,58 @@ const CNAdashboard = () => {
     <>
       <div>
         <NavBar />
-        <header>
-          <h1> CNA Dashboard</h1>
-          <h1>{name ? `Welcome ${name}` : ""}</h1>
+        <header className="flex justify-center">
+          <h1 className="font-semibold text-burnt text-3xl my-10">
+            {" "}
+            CNA Dashboard
+          </h1>
         </header>
       </div>
-
-      <div className="grid md:grid-cols-3 gap-4 justify-items-center md:mt-64">
-        <div className="block rounded-lg bg-orange-100 p-6 shadow-lg dark:bg-neutral-700 w-[300px]">
-          <h5 className="mb-10 text-xl font-body leading-tight text-neutral-800 dark:text-neutral-50 text-center">
-            Weekly Weight Sheet
-          </h5>
-          <div className="flex justify-center">
-            <Link to="/weeklysheet">
-              <button
-                type="button"
-                className="rounded bg-sky-600 hover:bg-primary px-6 pt-2.5 pb-2 font-body text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-              >
-                View
-              </button>
-            </Link>
-          </div>
+      <div className="grid grid-cols-2 justify-items-center mb-[150px]">
+        <div className="col-span-1">
+          <h1 className=" flex-col text-2xl font-semibold text-stone-600">
+            {name ? `Welcome ${name}` : ""}
+          </h1>
         </div>
-        <div className="block max-w-sm rounded-lg bg-orange-100 p-6 shadow-lg dark:bg-neutral-700 w-[300px]">
-          <h5 className="mb-10 text-xl font-body leading-tight text-neutral-800 dark:text-neutral-50 text-center">
-            Census List
-          </h5>
-          <div className="flex justify-center">
-            <Link to="/censuslist">
-              <button
-                type="button"
-                className="rounded bg-sky-600 hover:bg-primary px-6 pt-2.5 pb-2 font-body text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-              >
-                View
-              </button>
-            </Link>
-          </div>
+        <div className="flex items-center gap-6 col-span-1">
+          <img src={message} alt="logo" className="block w-16" />
+          <h3>You have 3 new messages</h3>
         </div>
-        <div className="block max-w-sm rounded-lg bg-orange-100 p-6 shadow-lg dark:bg-neutral-700 w-[300px]">
-          <h5 className="mb-10 text-xl font-body leading-tight text-neutral-800 dark:text-neutral-50 text-center">
-            My Messages
-          </h5>
-          <div className="flex justify-center">
-            <Link to="/inbox">
-              <button
-                type="button"
-                className="rounded bg-sky-600 hover:bg-primary px-6 pt-2.5 pb-2 font-body text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-              >
-                View
-              </button>
-            </Link>
+      </div>
+      <div className="grid md:grid-cols-3 gap-10 md:gap-0 justify-items-center">
+        <Link to="/weeklysheet">
+          <div className="flex-col  content-center rounded-lg bg-amber-200/70 p-6 shadow-xl dark:bg-neutral-700 w-[300px]">
+            <h5 className="mb-5 text-xl font-body leading-tight  text-sky-800 text-center">
+              Weekly Weight Sheet
+            </h5>
+            <div className="flex justify-center mb-5">
+              <img src={scale} alt="logo" className="block w-16" />
+            </div>
+            <div className="flex justify-center"></div>
           </div>
-        </div>
+        </Link>
+        <Link to="/censuslist">
+          <div className="flex-col  content-center rounded-lg bg-amber-200/70 p-6 shadow-xl dark:bg-neutral-700 w-[300px]">
+            <h5 className="mb-5 text-xl font-body leading-tight  text-sky-800 text-center">
+              Census List
+            </h5>
+            <div className="flex justify-center mb-5">
+              <img src={list} alt="logo" className="block w-16" />
+            </div>
+            <div className="flex justify-center"></div>
+          </div>
+        </Link>
+        <Link to="/inbox">
+          <div className="flex-col  content-center rounded-lg  bg-amber-200/70 p-6 shadow-xl dark:bg-neutral-700 w-[300px] mb-16">
+            <h5 className="mb-5 text-xl font-body leading-tight text-sky-800 text-center">
+              My Messages
+            </h5>
+            <div className="flex justify-center mb-5">
+              <img src={message} alt="logo" className="block w-16" />
+            </div>
+            <div className="flex justify-center"></div>
+          </div>
+        </Link>
       </div>
     </>
   );
