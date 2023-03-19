f38d515c9eb15f522dcf6bcffc4251deb285cd7e
Maia Dutta <mermaia33@gmail.com>
2023-03-18 21:44:11 -0500

    added header to RD dashboard

--- src/components/dashboard/RDdashboard.js
+++ src/components/dashboard/RDdashboard.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { alert } from "../../assets";
 import NavBar from "../nav/NavBar";
+import "./Dashboard.css";
 
 const RDdashboard = () => {
   const navigate = useNavigate();
@@ -15,15 +17,42 @@ const RDdashboard = () => {
     }
   }, [loggedIn]);
 
-  return (
-    <div>
-      <NavBar />
-      <header>
-        <h1> RD dashboard</h1>
-        <h1>{name ? `Welcome ${name}` : ""}</h1>
-      </header>
-    </div>
-  );
+ return (
+   <>
+     <div>
+       <NavBar />
+       <header className="flex justify-center">
+         <h1 className="font-semibold text-stone-700 text-3xl my-10">
+           {" "}
+           RD Dashboard
+         </h1>
+       </header>
+     </div>
+     <div className="text-center med:mt-5 med:mb-5 mb-10">
+       <h1 className="text-2xl font-semibold text-stone-600">
+         {name ? `Welcome ${name}` : ""}
+       </h1>
+     </div>
+     <div className="grid grid-cols-2 justify-items-center md:justify-items-stretch">
+       <div className="md:ml-40">
+         <h1 className="text-2xl font-semibold text-stone-600">Resources</h1>
+       </div>
+       <div className="flex items-center gap-2 md:gap-4 justify-center md:justify-end text-stone-700 md:mr-40">
+         <img src={alert} alt="logo" className="block  w-8 md:w-14" />
+         <h3>
+           You have <span className="font-bold">3</span>{" "}
+           <Link to="/inbox">
+             {" "}
+             <span className="text-sky-700 underline">new messages</span>
+           </Link>
+         </h3>
+       </div>
+     </div>
+     <hr className="rd-hr"></hr>
+   </>
+ );
+
+
 };
 
 export default RDdashboard;
