3f9277c90ebcd722f49c1093fdda6caed9c537b6
Maia Dutta <mermaia33@gmail.com>
2023-03-18 20:16:27 -0500

    added links to rn/np/md dashboard

--- src/components/dashboard/RNdashboard.js
+++ src/components/dashboard/RNdashboard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { scale, list, message, alert } from "../../assets";
+
 import NavBar from "../nav/NavBar";
 
 const RNdashboard = () => {
@@ -15,15 +17,67 @@ const RNdashboard = () => {
     }
   }, [loggedIn]);
 
-  return (
-    <div>
-      <NavBar />
-      <header>
-        <h1> WeightTrack dashboard</h1>
-        <h1>{name ? `Welcome ${name}` : ""}</h1>
-      </header>
-    </div>
-  );
+   return (
+     <>
+       <div>
+         <NavBar />
+         <header className="flex justify-center">
+           <h1 className="font-semibold text-stone-700 text-3xl my-10">
+             {" "}
+             Dashboard
+           </h1>
+         </header>
+       </div>
+       <div className="text-center med:mt-5 med:mb-5 mb-10">
+         <h1 className="text-2xl font-semibold text-stone-600">
+           {name ? `Welcome ${name}` : ""}
+         </h1>
+       </div>
+       <div className="flex items-center gap-2 md:gap-4 justify-center md:justify-end  md:mr-60 mt-5 mb-12 md:mb-[125px] text-stone-700">
+         <img src={alert} alt="logo" className="block  w-8 md:w-14" />
+         <h3>
+           You have <span className="font-bold">3</span>{" "}
+           <Link to="/inbox">
+             {" "}
+             <span className="text-sky-700 underline">new messages</span>
+           </Link>
+         </h3>
+       </div>
+       <div className="grid lg:grid-cols-3 gap-10 lg:gap-0 justify-items-center lg:m-auto lg:w-2/3">
+         <Link to="/weightsummary">
+           <div className=" rounded-lg p-6 shadow-lg border-2 bg-amber-100/60 w-[300px]">
+             <h5 className="mb-5 text-xl font-body leading-tight  text-sky-800 font-semibold text-center">
+               Weekly Weight Sheet
+             </h5>
+             <div className="flex justify-center mb-5">
+               <img src={scale} alt="logo" className="block w-16" />
+             </div>
+           </div>
+         </Link>
+         <Link to="/censuslist">
+           <div className="rounded-lg p-6 bg-amber-100/60 shadow-lg border-2 w-[300px]">
+             <h5 className="mb-5 text-xl font-body leading-tight text-sky-800 font-semibold text-center">
+               Census List
+             </h5>
+             <div className="flex justify-center mb-5">
+               <img src={list} alt="logo" className="block w-16" />
+             </div>
+           </div>
+         </Link>
+         <Link to="/inbox">
+           <div className="rounded-lg p-6 shadow-lg border-2 bg-amber-100/60 w-[300px] mb-16">
+             <h5 className="mb-5 text-xl font-body leading-tight text-sky-800 font-semibold text-center">
+               My Messages
+             </h5>
+             <div className="flex justify-center mb-5">
+               <img src={message} alt="logo" className="block w-16" />
+             </div>
+           </div>
+         </Link>
+       </div>
+     </>
+   );
+ 
 };
 
 export default RNdashboard;
