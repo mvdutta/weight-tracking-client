68baeb3381aa3a684c59d364881d77301e42df15
Maia Dutta <mermaia33@gmail.com>
2023-03-23 18:03:33 -0500

    styled message modal

--- src/components/messages/MessageDetail/MessageDetailModal.js
+++ src/components/messages/MessageDetail/MessageDetailModal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { close } from "../../../assets";
 
 const MessageDetailModal = ({showModal, setShowModal, email}) => {
 
@@ -12,37 +13,32 @@ const MessageDetailModal = ({showModal, setShowModal, email}) => {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-stone-200 rounded-t">
-                  <h3 className="text-2xl font-semibold">{email.message.subject}</h3>
+                  <h3 className="text-xl font-body text-stone-700 font-semibold">
+                    {email.message.subject}
+                  </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 ml-auto float-right leading-none outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      close
+                    <span className="">
+                      <img className="w-7" src={close} alt="close-icon" />
                     </span>
                   </button>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-stone-500 text-lg leading-relaxed">
-                  {email.message.message_body}
+                  <p className="my-4 text-stone-600 text-lg leading-relaxed">
+                    {email.message.message_body}
                   </p>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-stone-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-amber-500/90 hover:bg-amber-400 py-2 px-3 md:py-3 md:px-4 text-sm md:text-md text-white rounded border focus:outline-none focus:border-black"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
                   </button>
                 </div>
               </div>
@@ -51,7 +47,6 @@ const MessageDetailModal = ({showModal, setShowModal, email}) => {
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
-
     </>
   );
 }
