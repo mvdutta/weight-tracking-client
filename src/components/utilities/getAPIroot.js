03eee7081a1623a4840cd81323abe6703398061b
Maia Dutta <mermaia33@gmail.com>
2023-05-28 16:21:12 -0500

    testing APIROOT

--- src/components/utilities/getAPIroot.js
+++ src/components/utilities/getAPIroot.js
@@ -5,5 +5,6 @@ export const getAPIroot = () => {
     //   ? "http://localhost:8000/"
     //   : "https://q72y0iroi2.execute-api.us-west-2.amazonaws.com/";
     const APIROOT = "https://q72y0iroi2.execute-api.us-west-2.amazonaws.com/";
+    console.log(APIROOT)
   return APIROOT;
 };
