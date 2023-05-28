fe8e99a8092a9c8eeb2a9058da03cb62fece1c8f
Maia Dutta <mermaia33@gmail.com>
2023-05-28 16:56:34 -0500

    attempting to automatically detect environment

--- src/components/utilities/getAPIroot.js
+++ src/components/utilities/getAPIroot.js
@@ -1,10 +1,9 @@
 export const getAPIroot = () => {
   const environment = process.env.NODE_ENV;
-  // const APIROOT =
-    // environment === "development"
-    //   ? "http://localhost:8000/"
-    //   : "https://q72y0iroi2.execute-api.us-west-2.amazonaws.com/";
-    const APIROOT = "https://q72y0iroi2.execute-api.us-west-2.amazonaws.com/";
-    console.log(APIROOT)
+  const APIROOT =
+    environment === "development"
+      ? "http://localhost:8000/"
+      : "https://q72y0iroi2.execute-api.us-west-2.amazonaws.com/";
+    // const APIROOT = "https://q72y0iroi2.execute-api.us-west-2.amazonaws.com/";
   return APIROOT;
 };
