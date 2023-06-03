8a3fdca967579083c45695ef830984368d630918
Maia Dutta <mermaia33@gmail.com>
2023-06-03 16:09:55 -0500

    small change to getAPIroot.js

--- src/components/utilities/getAPIroot.js
+++ src/components/utilities/getAPIroot.js
@@ -1,10 +1,9 @@
 export const getAPIroot = () => {
   const environment = process.env.NODE_ENV;
-  // const APIROOT =
-  //   environment === "development"
-  //     ? "http://localhost:8000/"
-  //     : "https://weighttracker-api.onrender.com/";
-    const APIROOT = "https://weighttracker-api.onrender.com/";
-    console.log(APIROOT)
+  const APIROOT =
+    environment === "development"
+      ? "http://localhost:8000/"
+      : "https://weighttracker-api.onrender.com/";
+    // const APIROOT = "https://weighttracker-api.onrender.com/";
   return APIROOT;
 };
