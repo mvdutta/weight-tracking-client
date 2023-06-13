08cdb755cca069d1c24c360805cfc3d7970d15b4
Maia Dutta <mermaia33@gmail.com>
2023-06-13 11:10:57 -0500

    changes made to resident weight summary search and results to accommodate smaller screen sizes

--- src/components/dashboard/RDdashboard.js
+++ src/components/dashboard/RDdashboard.js
@@ -6,16 +6,20 @@ import { fetchIt } from "../auth/fetchIt";
 import NavBar from "../nav/NavBar";
 import "./Dashboard.css";
 import WeightSheetMenuModal from "../weightSheets/WeightSheetMenuModal";
-import { formattedDate, formattedDateMDY, formattedDateUI } from "../utilities/FormattedDate";
+import {
+  formattedDate,
+  formattedDateMDY,
+  formattedDateUI,
+} from "../utilities/FormattedDate";
 import { Graph } from "./Graph";
 import { getAPIroot } from "../utilities/getAPIroot";
 
 const formatDecimal = (x) => {
-  if (x== undefined || isNaN(x)) {
-    return ""
+  if (x === undefined || isNaN(x)) {
+    return "";
   }
-  return Number.parseFloat(x).toFixed(2)
-}
+  return Number.parseFloat(x).toFixed(2);
+};
 
 const RDdashboard = () => {
   const navigate = useNavigate();
@@ -56,11 +60,9 @@ const RDdashboard = () => {
     const searchTerm = e.target.value;
     if (searchTerm.length > 0) {
       setShowSearchResults(true);
-      fetchIt(`${APIROOT}residents?keyword=${searchTerm}`).then(
-        (data) => {
-          setSearchResults(data);
-        }
-      );
+      fetchIt(`${APIROOT}residents?keyword=${searchTerm}`).then((data) => {
+        setSearchResults(data);
+      });
     } else {
       setShowSearchResults(false);
     }
@@ -83,12 +85,9 @@ const RDdashboard = () => {
         <tbody>
           <tr
             key={`table-row-${0}`}
-            className="bg-white border-b text-sm sm:text-base text-stone-700  dark:bg-stone-800 dark:border-stone-800 hover:bg-stone-50 dark:hover:bg-stone-600"
+            className="bg-white border-b text-sm sm:text-base text-stone-700 dark:bg-stone-800 dark:border-stone-800 hover:bg-stone-50 dark:hover:bg-stone-600"
           >
-            <td
-              scope="row"
-              className="px-6 py-4 whitespace-nowrap dark:text-white"
-            >
+            <td className="px-6 py-4 whitespace-nowrap dark:text-white">
               {formatDecimal(weightData.ABW)}
             </td>
             <td className="px-6 py-4">{formatDecimal(PBW)}</td>
@@ -114,32 +113,34 @@ const RDdashboard = () => {
         </h1>
       </header>
       <div className="text-center md:mt-5 mb-10 md:mb-28">
-        <h1 className="text-2xl text-stone-600">
+        <h1 className=" text-xl sm:text-2xl text-stone-600">
           {name ? `Welcome ${name}` : ""}
         </h1>
       </div>
-      <div className="grid grid-cols-3 md:ml-auto md:mr-auto text-md md:text-lg md:justify-items-center mb-20 md:w-2/3">
-        <div className="flex items-center gap-1 md:gap-2 justify-center text-stone-700 hover:underline ">
+      <div className="grid grid-rows-3 gap-3 sm:gap-6 md:grid-cols-3 ml-auto mr-auto text-sm sm:text-lg justify-items-start sm:justify-items-center w-[170px] md:w-[700px] lg:w-2/3 mb-14 sm:mb-2">
+        <div className="flex items-center gap-2 text-stone-700 hover:underline ">
           <a
             href="https://mvdutta.github.io/metabolic-calculator/"
             target="_blank"
+            rel="noreferrer"
           >
             <img
               src={calculator}
               alt="calculator"
-              className="block opacity-80 w-6 md:w-12"
+              className="block opacity-80 w-8 md:w-12"
             />
           </a>
           <a
             href="https://mvdutta.github.io/metabolic-calculator/"
             target="_blank"
-            className="w-[100px]"
+            rel="noreferrer"
+            className=""
           >
             Calculator
           </a>
         </div>
         <div
-          className="flex items-center gap-1 md:gap-2 justify-left text-stone-700 hover:underline cursor-pointer -ml-4 sm:ml-0"
+          className="flex items-center gap-2 md:gap-2 text-stone-700 hover:underline cursor-pointer"
           onClick={() => {
             setShowModal(true);
           }}
@@ -147,29 +148,29 @@ const RDdashboard = () => {
           <img
             src={rdclipboard}
             alt="rdclipboard"
-            className="block opacity-80 w-6 md:w-12"
+            className="block opacity-80 w-8 md:w-12"
           />
           <span className="">Weight Sheets</span>
         </div>
         <Link to="/inbox">
-          <div className="flex items-center gap-1 md:gap-4 text-stone-700 -ml-6  sm:ml-0 cursor-pointer">
+          <div className="flex items-center gap-2 md:gap-4 text-stone-700 cursor-pointer">
             <img
               src={alert}
               alt="alert"
-              className="block opacity-80 w-6 md:w-12"
+              className="block opacity-80 w-8 md:w-12"
             />
-            <h3 className="">
-              <span className="hidden xl:inline-block">You have </span>{" "}
-              <span className="font-bold text-sky-900">{numUnreadMsgs}</span>{" "}
-              <span className="text-stone-700 hover:underline">
+            <h3 className="flex sm:block">
+              <span className="hidden lg:inline-block">You have </span>{" "}
+              <span className="font-semibold text-sky-900 sm:text-lg">
+                {numUnreadMsgs} 
+              </span>{" "}
+              <span className="text-stone-700 hover:underline ml-[2.6px] lg:ml-0">
                 unread message(s)
               </span>
             </h3>
           </div>
         </Link>
       </div>
-      {/* mobile menu */}
-      
       <hr className="rd-hr"></hr>
 
       <header className="flex justify-center">
@@ -179,17 +180,17 @@ const RDdashboard = () => {
         </h1>
       </header>
 
-      <div className="flex items-center ml-10 sm:ml-20 md:ml-60">
-        <div>
-          <h1 className="text-stone-600 font-semibold text-lg">
+      <div className="flex items-center justify-start gap-2 sm:gap-0 mt-5 sm:mt-10 ml-5 sm:ml-10 lg:ml-60">
+        <div className="">
+          <h1 className="text-stone-600 font-semibold sm:text-lg">
             Search by Resident:
           </h1>
         </div>
         <div>
-          <form className="max-w-sm px-2">
+          <form className=" w-[175px]  sm:w-full sm:px-4">
             <div className="relative">
               <svg
-                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                className="absolute top-0 bottom-0 w-5 sm:w-6 h-5 sm:h-6 my-auto text-gray-400 left-3"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -207,14 +208,14 @@ const RDdashboard = () => {
                 onChange={(e) => {
                   residentSearch(e);
                 }}
-                className=" w-full py-1 pl-12 pr-4 text-stone-700 border-2 border-stone-300 rounded-md  bg-stone-100 focus:bg-white focus:border-sky-600"
+                className="w-full py-1 pl-12 pr-4 text-sm sm:text-lg text-stone-700 text-md border-2 border-stone-300 rounded-md  bg-stone-100 focus:bg-white focus:border-stone-400"
               />
             </div>
           </form>
         </div>
       </div>
       {showSearchResults ? (
-        <div className="inline-block border-2 border-stone-200/60 rounded-md p-2 mt-5 ml-10 sm:ml-20  md:ml-60">
+        <div className="inline-block border-2 border-stone-200/60 rounded-md p-2 mt-5 mb-10 ml-5 sm:ml-10 lg:ml-60">
           {searchResults.map((result) => {
             return (
               <div key={`result--${result.id}`} className="">
@@ -238,7 +239,7 @@ const RDdashboard = () => {
         ""
       ) : (
         <div>
-          <h1 className="text-lg ml-10 sm:ml-20 md:ml-60 mt-5 mb-10 text-stone-800">
+          <h1 className="text-lg ml-5 sm:ml-10 lg:ml-60 mt-5 mb-10 text-stone-800">
             Weight Summary for:{" "}
             <span className="italic text-stone-700 ">
               {selectedResident.first_name} {selectedResident.last_name}
@@ -249,7 +250,7 @@ const RDdashboard = () => {
       {searchResults.length === 0 ? (
         ""
       ) : (
-        <div className=" container flex flex-col m-auto shadow-md sm:rounded-lg md:w-1/2 font-body border border-sky-600/30 pb-5 px-4 text-sm md:text-lg overflow-x-scroll">
+        <div className=" container flex flex-col m-auto sm:shadow-md sm:rounded-lg w-full lg:w-2/3 font-body border border-stone-600/30 pb-5 px-4 text-sm md:text-lg overflow-x-scroll">
           <div className="w-auto overflow-x-auto">
             <table className="text-md text-stone-700 dark:text-stone-500">
               <caption className="p-5 text-lg font-semibold text-stone-800 bg-white dark:text-white dark:bg-gray-800">
@@ -266,7 +267,7 @@ const RDdashboard = () => {
                   </p>
                 </div>
               </caption>
-              <thead className="text-xs sm:text-sm text-sky-900  font-semibold bg-stone-200 dark:bg-stone-700 dark:text-stone-400">
+              <thead className="text-xs sm:text-sm text-sky-900 font-semibold bg-stone-200 dark:bg-stone-700 dark:text-stone-400">
                 <tr>
                   <th scope="col" className="px-2 py-2 sm:px-6 sm:py-3">
                     ABW (lbs)
@@ -310,7 +311,6 @@ const RDdashboard = () => {
         </div>
       )}
       <WeightSheetMenuModal showModal={showModal} setShowModal={setShowModal} />
-      {/* <Graph id={1}/> */}
       {selectedResident && selectedResident.id ? (
         <Graph
           id={selectedResident?.id}
